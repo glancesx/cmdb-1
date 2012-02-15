96336e328d3951bdc43f32f5fdbab68701248846
maxfox <yezi83618@gmail.com>
2012-02-15 23:58:57 +0800

    db package added

---
+++ cmdb/dal/appInstanceDAO.py
@@ -0,0 +1,18 @@
+'''
+Created on 2012-2-13
+
+@author: yezi
+'''
+
+class appInstanceDAO(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        return;
+        
---
+++ cmdb/dal/appSourceDAO.py
@@ -0,0 +1,29 @@
+'''
+Created on 2012-2-13
+
+@author: yezi
+'''
+
+class appSourceDAO(object):
+    '''
+    classdocs AppSource's database operation
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        return;
+    
+    def insertAppSource(self):
+        return;
+        
+    def deleteAppSource(self):
+        return;
+    
+    def queryAppSource(self):
+        return;
+    
+    def updateAppSource(self):
+        return;
