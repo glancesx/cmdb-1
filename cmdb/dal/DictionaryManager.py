ad5edbf33fb79c412869db7d1c367517373c8ac1
maxfox <yezi83618@gmail.com>
2012-02-26 22:57:28 +0800

    AppDictionary code

--- cmdb/dal/DictionaryManager.py
+++ cmdb/dal/DictionaryManager.py
@@ -3,7 +3,7 @@ Created on 2012-2-25
 
 @author: zi.yez
 '''
-from models import CMDB_Dictionary
+from models import CMDB_Dictionary,KEY_TYPE_CHOICES
 
 class DictionaryManager(object):
     '''
@@ -17,27 +17,64 @@ class DictionaryManager(object):
     
     def getDictionaryInfoAll(self):               
         dictionary = CMDB_Dictionary()
-        return dictionary.objects.all()
+        return dictionary.objects.filter(flag = True)
     
+    def getDictionaryInfoAllFalse(self):
+        dictionary = CMDB_Dictionary()
+        return dictionary.objects.filter(flag = False)
+           
     def getDictionaryInfoByType(self,keyType):
         if keyType is None:
             #add logging
             return        
         dictionary = CMDB_Dictionary()
-        return dictionary.objects.filter(key_type = keyType)
+        return dictionary.objects.filter(key_type = keyType,flag = True)
     
     def getDictionaryInfoByValue(self,keyValue):
         if keyValue is None:
             #add logging
             return
         dictionary = CMDB_Dictionary()
-        return dictionary.objects.filter(value__contains = keyValue)
+        return dictionary.objects.filter(value__contains = keyValue,flag = True)
         
     def insertDictionaryInfo(self,dictionaryList):
         dictionary = CMDB_Dictionary()
         for dictionary in dictionaryList:
+            if self.checkKeyType(dictionary.key_type):
+                #add logging
+                return    
             dictionary.save()
+                
+    def updateDictionaryInfo(self,dictionaryInfo):
+        if self.checkKeyType(dictionaryInfo.key_type):
+            #add logging
+            return
+        
+        dictionary = CMDB_Dictionary()
+        existDict = dictionary.objects.get(id = dictionaryInfo.id)
+        if existDict :
+            updateDict = existDict(dictionaryInfo)
+            updateDict.save()
+        else:
+            #add logging
+            return
             
+    def switchDictionaryInfoFlag(self,keyId,flagStatus):
+        dictionary = CMDB_Dictionary()
+        existDict = dictionary.objects.get(id = keyId)
+        if existDict:
+            updateDict = existDict(flag = flagStatus)
+            updateDict.save()
+        else :
+            #add logging
+            return
+    
+    def checkKeyType(self,keyType):
+        checkFlag = True
+        for keyTuple in KEY_TYPE_CHOICES:
+            if keyType == keyTuple(0):
+                checkFlag = False
+        return checkFlag
     
             
             
--- cmdb/dal/models.py
+++ cmdb/dal/models.py
@@ -9,24 +9,25 @@ KEY_TYPE_CHOICES = (
                     (u'PATITION_TYPE',u'patition_type class'),
                     (u'ENV',u'env class'),
                     (u'APP',u'app class'),
-                    );
+                    )
 
 class Common(models.Model):
-    gmtCreator = models.CharField(max_length = 32);
-    gmtCreated = models.DateTimeField(auto_now_add = True);
-    gmtModifier = models.CharField(max_length = 32);
-    gmtModified = models.DateTimeField(auto_now = True);
+    gmtCreator = models.CharField(max_length = 32)
+    gmtCreated = models.DateTimeField(auto_now_add = True,input_formats ='%Y-%m-%d %H:%M:%S')
+    gmtModifier = models.CharField(max_length = 32)
+    gmtModified = models.DateTimeField(auto_now = True,input_formats ='%Y-%m-%d %H:%M:%S')
+    flag = models.BooleanField(required = False)
     
     class Meta:
         app_label = 'dal'
-        abstract = True;
+        abstract = True
         
 # Create your models here.
 # 服务器字典表
 class CMDB_Dictionary(Common):
-    key = models.CharField(max_length = 20,unique = True);
-    value = models.CharField(max_length = 50);
-    key_type = models.CharField(max_length = 20,choices = KEY_TYPE_CHOICES);
+    key = models.CharField(max_length = 20)
+    value = models.CharField(max_length = 50)
+    key_type = models.CharField(max_length = 20,choices = KEY_TYPE_CHOICES)
     
     class Meta:
         app_label = 'dal'
@@ -34,52 +35,52 @@ class CMDB_Dictionary(Common):
 
 # 服务器硬件资源表
 class CMDB_AppServer(Common):
-    host_name = models.CharField(max_length = 30, unique = True);
-    cpu_core = models.CharField(max_length = 10);
-    cpu_type = models.ForeignKey(CMDB_Dictionary);
-    memory = models.CharField(max_length = 30);
-    sn = models.CharField(max_length = 30);
+    host_name = models.CharField(max_length = 30)
+    cpu_core = models.CharField(max_length = 10)
+    cpu_type = models.ForeignKey(CMDB_Dictionary)
+    memory = models.CharField(max_length = 30)
+    sn = models.CharField(max_length = 30)
     
 # 服务器实例资源表   
 class CMDB_AppInstance(Common):
-    host_name = models.CharField(max_length = 30,unique = True);
-    cpu_core = models.CharField(max_length = 30);
-    memory = models.CharField(max_length = 30);
-    appserver_id = models.ForeignKey(CMDB_AppServer); 
+    host_name = models.CharField(max_length = 30)
+    cpu_core = models.CharField(max_length = 30)
+    memory = models.CharField(max_length = 30)
+    appserver_id = models.ForeignKey(CMDB_AppServer) 
     
 # IP资源表
 class CMDB_Ip_Source(Common):
-    ip = models.CharField(max_length = 39);
-    ip_type = models.ForeignKey(CMDB_Dictionary);
-    server = models.IntegerField();
-    server_type = models.CharField(max_length = 20);
+    ip = models.CharField(max_length = 39)
+    ip_type = models.ForeignKey(CMDB_Dictionary)
+    server = models.IntegerField()
+    server_type = models.CharField(max_length = 20)
     
 # 硬盘硬件资源表
 class CMDB_Disc_Source(Common):
-    number = models.IntegerField();
-    size = models.BigIntegerField();
-    raid = models.ForeignKey(CMDB_Dictionary);
-    raid_size = models.BigIntegerField();
-    remark = models.CharField(max_length = 500);
-    appserver_id = models.ForeignKey(CMDB_AppServer);
+    number = models.IntegerField()
+    size = models.BigIntegerField()
+    raid = models.ForeignKey(CMDB_Dictionary)
+    raid_size = models.BigIntegerField()
+    remark = models.CharField(max_length = 500)
+    appserver_id = models.ForeignKey(CMDB_AppServer)
 
 # 服务器实例分区资源表
 class CMDB_Disc_Patition(Common):
-    patition = models.CharField(max_length = 50);
-    patition_size = models.BigIntegerField();
-    patition_type = models.ForeignKey(CMDB_Dictionary);
-    appinstance_id = models.ForeignKey(CMDB_AppInstance);
+    patition = models.CharField(max_length = 50)
+    patition_size = models.BigIntegerField()
+    patition_type = models.ForeignKey(CMDB_Dictionary)
+    appinstance_id = models.ForeignKey(CMDB_AppInstance)
 
 # 服务器应用系统关联表    
 class CMDB_AppBiz(Common):
-    env = models.ForeignKey(CMDB_Dictionary,related_name = 'env_set');
-    app = models.ForeignKey(CMDB_Dictionary,related_name = 'app_set');
-    app_source = models.URLField();
-    appinstance_id = models.ForeignKey(CMDB_AppInstance);
+    env = models.ForeignKey(CMDB_Dictionary,related_name = 'env_set')
+    app = models.ForeignKey(CMDB_Dictionary,related_name = 'app_set')
+    app_source = models.URLField()
+    appinstance_id = models.ForeignKey(CMDB_AppInstance)
     
 # DNS解析表
 class CMDB_Dns(Common):
-    ip = models.CharField(max_length = 39);
-    dns = models.URLField();
-    env = models.ForeignKey(CMDB_Dictionary,related_name = 'env_dns_set');
-    app = models.ForeignKey(CMDB_Dictionary,related_name = 'app_dns_set');
+    ip = models.CharField(max_length = 39)
+    dns = models.URLField()
+    env = models.ForeignKey(CMDB_Dictionary,related_name = 'env_dns_set')
+    app = models.ForeignKey(CMDB_Dictionary,related_name = 'app_dns_set')
